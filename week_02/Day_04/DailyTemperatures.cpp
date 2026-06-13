f1d6aacaa257c520c99ce3049a3cb73c66d2c95d
muskankumari68767-sys <muskansingh68767@gmail.com>
2026-06-13 20:48:45 +0530

    Implement dailyTemperatures function in DailyTemperatures.cpp

---
+++ week_02/Day_04/DailyTemperatures.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+        vector<int> ans(temperatures.size(),0);
+
+        for(int i=0;i<temperatures.size();i++) {
+
+            for(int j=i+1;j<temperatures.size();j++) {
+
+                if(temperatures[j] > temperatures[i]) {
+                    ans[i] = j-i;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};

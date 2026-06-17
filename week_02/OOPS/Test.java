688b4b7d7930fd78721f8fed7fdbd1959234cc15
muskankumari68767-sys <muskansingh68767@gmail.com>
2026-06-17 11:07:13 +0530

    Add Test class with main method for Playable interface

---
+++ week_02/OOPS/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        Veena v = new Veena();
+
+        v.play();
+
+        Saxophone s = new Saxophone();
+
+        s.play();
+
+        Playable p;
+
+        p = v;
+
+        p.play();
+
+        p = s;
+
+        p.play();
+    }
+}
